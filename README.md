68c388d810e036510376ff2c564fba6c70e231ba
brian <balsamo@uchicago.edu>
2017-11-01 15:57:57 -0500

    binding to 80 and adding versioning

--- README.md
+++ README.md
@@ -1,3 +1,9 @@
 # docker-loris
 
+## v0.0.1
+
 A dockerized loris server hosted via uwsgi.
+
+# Runtime EnvArgs
+
+- IMG_ROOT: The root directory image identifiers should be resolved from.
--- entrypoint.sh
+++ entrypoint.sh
@@ -1,3 +1,3 @@
 #!/bin/sh
 
-sed -i "s/^    src_img_root = .*$/    src_img_root = '${IMG_ROOT:-\/usr\/local\/share\/images}'/g" /etc/loris2/loris2.conf && uwsgi --http :5000 --wsgi-file /var/www/loris2/loris2.wsgi --master --processes 2 --threads 2
+sed -i "s/^    src_img_root = .*$/    src_img_root = '${IMG_ROOT:-\/usr\/local\/share\/images}'/g" /etc/loris2/loris2.conf && uwsgi --http :80 --wsgi-file /var/www/loris2/loris2.wsgi --master --processes 2 --threads 2
